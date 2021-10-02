6883403e861c48717ebd03b9935a6d17ea51ee45
bitIO <francisco.calle.moreno@gmail.com>
2021-10-02 12:10:14 +0200

    fix: only report web vitals in dev

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
@@ -15,4 +15,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  reportWebVitals(console.log);
+}
